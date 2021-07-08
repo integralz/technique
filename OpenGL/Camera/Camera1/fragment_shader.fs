57514f9249bb8626c038f4b41e37c8231979f480
integralz <luckystay12@gmail.com>
2021-07-08 14:58:39 +0900

    camera1

---
+++ OpenGL/Camera/Camera1/fragment_shader.fs
@@ -0,0 +1,11 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 textcoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+void main()
+{
+    FragColor = mix(texture(texture1, textcoord), texture(texture2, textcoord), 0.3);
+}
