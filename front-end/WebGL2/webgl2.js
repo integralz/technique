3fc68fe2bcfaab3cb6394968df4cceeecf926349
integralz <luckystay12@gmail.com>
2021-08-04 15:32:47 +0900

    webgl2 template

---
+++ front-end/WebGL2/webgl2.js
@@ -0,0 +1,55 @@
+"use strict"
+
+var vertexShaderSource = `#version 300 es
+
+`;
+
+var fragmentShaderSource = `#version 300 es
+
+`;
+
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function main() {
+    var canvas = document.querySelector("#webgl_example");
+
+    var gl = canvas.getContext("webgl2");
+    if (!gl) {
+        console.log("no gl!");
+    }
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    var program = createProgram(gl, vertexShader, fragmentShader);
+}
+
+main();
+
+
